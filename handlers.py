e72eda2842bfd4b404babf098f61c6cdfe6428b8
beberich <svdadm165@gmail.com>
2024-10-21 22:25:49 +0300

    Bug fixes for incorrect entry of dates and transaction amounts

--- handlers.py
+++ handlers.py
@@ -2,10 +2,12 @@ from aiogram import types
 from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command, Text
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import Message, MediaGroup
+from aiogram.types import InputMediaDocument
 from aiogram.dispatcher.filters.state import State, StatesGroup
+import re
+from datetime import datetime
 
-# Хранение данных о доходах и расходах
 incomes = []
 expenses = []
 income_categories = ['Зарплата', 'Продажа', 'Другое']
@@ -84,16 +86,23 @@ async def custom_income_category(message: types.Message, state: FSMContext):
 
 
 async def save_income(message: types.Message, state: FSMContext):
-    try:
-        income_amount = float(message.text)
-        async with state.proxy() as data:
-            category = data.get('custom_income_category', message.text)
-            incomes.append({"category": category, "amount": income_amount})
-        await message.answer("Отлично! Ваш доход записан.")
-    except ValueError:
-        await message.answer("Введите корректную сумму.")
-    await state.finish()
-    await begin_command(message)
+    if message.text == 'Назад':
+        await state.finish()
+        await begin_command(message)
+    else:
+        try:
+            income_amount = float(message.text)
+            async with state.proxy() as data:
+                category = data.get('custom_income_category', message.text)
+                incomes.append({"category": category, "amount": income_amount})
+            await message.answer("Отлично! Ваш доход записан.")
+            await state.finish()
+            await begin_command(message)
+        except ValueError:
+            kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            kb.add("Назад")
+            await message.answer("Введите корректную сумму:", reply_markup=kb)
+            await Form.add_expense_amount.set()
 
 
 async def custom_expense_category(message: types.Message, state: FSMContext):
@@ -104,31 +113,63 @@ async def custom_expense_category(message: types.Message, state: FSMContext):
 
 
 async def save_expense(message: types.Message, state: FSMContext):
-    try:
-        expense_amount = float(message.text)
-        async with state.proxy() as data:
-            category = data.get('custom_expense_category', message.text)
-            expenses.append({"category": category, "amount": expense_amount})
-        await message.answer("Отлично! Ваш расход записан.")
-    except ValueError:
-        await message.answer("Введите корректную сумму.")
-    await state.finish()
-    await begin_command(message)
+    if message.text == 'Назад':
+        await state.finish()
+        await begin_command(message)
+    else:
+        try:
+            expense_amount = float(message.text)
+            async with state.proxy() as data:
+                category = data.get('custom_expense_category', message.text)
+                expenses.append({"category": category, "amount": expense_amount})
+            await message.answer("Отлично! Ваш расход записан.")
+            await state.finish()
+            await begin_command(message)
+        except ValueError:
+            kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            kb.add("Назад")
+            await message.answer("Введите корректную сумму:", reply_markup=kb)
+            await Form.add_expense_amount.set()
 
 
 async def get_statement_start_date(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['start_date'] = message.text
-    await message.answer("Введите дату окончания (dd.mm.yyyy):")
-    await Form.statement_end_date.set()
+    if re.search("^(0[0-9]|1[0-9]|2[0-9]|3[0-1])(.|-)(0[1-9]|1[0-2])(.|-|)20[0-9][0-9]$", data['start_date']):
+        await message.answer("Введите дату окончания (dd.mm.yyyy):")
+        await Form.statement_end_date.set()
+    else:
+        if message.text == "Назад":
+            await state.finish()
+            await begin_command(message)
+        else:
+            kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            kb.add("Назад")
+            await message.answer("Введите корректную дату начала", reply_markup=kb)
+            await Form.statement_start_date.set()
 
 
 async def get_statement_end_date(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['end_date'] = message.text
-        await message.answer("Получена выписка в формате PDF.")
-    await state.finish()
-    await begin_command(message)
+    if re.search("^(0[0-9]|1[0-9]|2[0-9]|3[0-1])(.|-)(0[1-9]|1[0-2])(.|-|)20[0-9][0-9]$",
+                 data['end_date']) and datetime.strptime(data['start_date'], '%d.%m.%Y') < datetime.strptime(
+            data['end_date'], '%d.%m.%Y'):
+        media = MediaGroup()
+        media.attach(InputMediaDocument(open('pdf.pdf', 'rb')))
+        await message.reply_media_group(media=media)
+        await message.answer('Выписка получена')
+        await state.finish()
+        await begin_command(message)
+    else:
+        if message.text == "Назад":
+            await state.finish()
+            await begin_command(message)
+        else:
+            kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            kb.add("Назад")
+            await message.answer("Введите корректную дату окончания", reply_markup=kb)
+            await Form.statement_end_date.set()
 
 
 def register_handlers(dp: Dispatcher):
